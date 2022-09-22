a2eccb30b715055c5f9a3c96c62311180310c1b6
HyperCodec <72839119+HyperCodec@users.noreply.github.com>
2022-09-22 16:15:22 -0400

    added proxy support

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
  Finds vanity links for servers
 
 ### Directions
+- add proxies to settings.py
 - run
 - check links.json after it is done
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import json, requests, itertools, time
+import json, requests, itertools, time, settings
 from requests import RequestException
 
 existinglinks = []
-
 chars = "abcdefghigjklmnopqrstuvwxyz!$&-_=+,1234567890"
+
+proxycycle = itertools.cycle(settings.proxies)
 
 for length in range(1, 17):
     for link in itertools.product(chars, repeat=length):
         link = ''.join(link)
+        proxy = next(proxycycle)
 
         try:
-            response = requests.get(f"https://discordapp.com/api/invites/https://discord.gg/{link}", allow_redirects=True)
+            response = requests.get(f"https://discordapp.com/api/invites/{link}", allow_redirects=True, proxies={"http": proxy, "https": proxy})
 
-            print(f"Connected to https://discordapp.com/api/invites/https://discord.gg/{link} with status code {response.status_code} ({response.elapsed})")
+            print(f"Connected to https://discordapp.com/api/invites/{link} with status code {response.status_code} ({response.elapsed})")
 
             if response.status_code == 200:
                 print(f"discord.gg/{link} is valid")
